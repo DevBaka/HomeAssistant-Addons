02019a22c3154d589338a4360df22d59e3a1be02
DJBaka <devbaka@protonmail.com>
2025-11-14 03:39:46 +0100

    gdfgdfgd

--- icecast/run.sh
+++ icecast/run.sh
@@ -2,6 +2,4 @@
 # ==============================================================================
 # Icecast Addon Start Script
 # ==============================================================================
-
-# Start Icecast as non-root user
 exec su -s /bin/bash icecast -c "icecast -c /etc/icecast2/icecast.xml"
