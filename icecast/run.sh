164614c1c1d96f5d7c7502b2f23e2adeab2b2454
DJBaka <devbaka@protonmail.com>
2025-11-14 03:22:11 +0100

    last versuche

--- icecast/run.sh
+++ icecast/run.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-# Use custom port if provided
-if [ ! -z "$PORT" ]; then
-    sed -i "s/<port>8000<\/port>/<port>$PORT<\/port>/" /etc/icecast2/icecast.xml
-fi
+# Set Icecast config file permissions
+chown icecast:icecast /etc/icecast2/icecast.xml
 
-# Start Icecast as icecast user
-exec su -s /bin/bash icecast -c "icecast -c /etc/icecast2/icecast.xml"
+# Start Icecast as the icecast user using changeowner
+exec icecast -c /etc/icecast2/icecast.xml
