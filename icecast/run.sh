e2e1ce7a7e813ed2490e3868f8fa2c90c5b629f1
DJBaka <devbaka@protonmail.com>
2025-11-14 03:36:51 +0100

    static

--- icecast/run.sh
+++ icecast/run.sh
@@ -1,25 +1,7 @@
-#!/usr/bin/env bash
-set -e
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Icecast Addon Start Script
+# ==============================================================================
 
-# default values if HA didn't pass them
-: "${BIND_IP:=0.0.0.0}"
-: "${PORT:=8000}"
-: "${MOUNTPOINT:=/stream}"
-: "${SOURCE_PASSWORD:=hackme}"
-: "${ADMIN_PASSWORD:=admin}"
-
-ICECAST_CONF="/etc/icecast2/icecast.xml"
-
-# replace placeholders
-sed -i "s|<BIND_IP>|${BIND_IP}|g" "$ICECAST_CONF"
-sed -i "s|<PORT>|${PORT}|g" "$ICECAST_CONF"
-sed -i "s|<MOUNTPOINT>|${MOUNTPOINT}|g" "$ICECAST_CONF"
-sed -i "s|<SOURCE_PASSWORD>|${SOURCE_PASSWORD}|g" "$ICECAST_CONF"
-sed -i "s|<ADMIN_PASSWORD>|${ADMIN_PASSWORD}|g" "$ICECAST_CONF"
-
-# ensure permissions
-chown icecast:icecast "$ICECAST_CONF"
-chown -R icecast:icecast /var/lib/icecast /var/log/icecast2
-
-# run icecast2 in foreground. icecast2 will drop privileges using <changeowner>.
-exec /usr/bin/icecast2 -n -c "$ICECAST_CONF"
+# Start Icecast as non-root user
+exec su -s /bin/bash icecast -c "icecast -c /etc/icecast2/icecast.xml"
