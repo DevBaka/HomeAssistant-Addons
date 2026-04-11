9d02e1af2dfca2223abcb850ff830f118f97500d
DevBaka <devbaka@protonmail.com>
2026-04-11 03:14:45 +0200

    fix speedtest

--- speedtest-tracker/config/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-
-    'rate_limit' => env('API_RATE_LIMIT', 60),
-
-];
--- speedtest-tracker/lang/es_ES/tools.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    // Ookla server list
-    'ookla_servers' => 'Servidores de Ookla',
-];
